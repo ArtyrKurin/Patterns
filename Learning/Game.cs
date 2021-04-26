70cabc9a953e5e02dae6a9f1c69d2dcb0dfcdbab
artyrkurin <kurinartyr@gmail.com>
2021-04-26 11:53:06 +0300

    TDD scoring bowling game

--- Learning/Game.cs
+++ Learning/Game.cs
@@ -2,15 +2,54 @@
 {
     public class Game
     {
-        private int score;
-        public int Score
+        private int currentFrame = 0;
+        private bool IsFistThrow = true;
+        private Scorer scorer = new Scorer();
+
+        public int CurrentFrame
         {
-            get { return 0; }
+            get { return currentFrame; }
         }
 
+        public int Score
+        {
+            get { return ScoreForFrame(currentFrame); }
+        }
+        private bool Strike(int pins)
+        {
+            return (IsFistThrow && pins == 10);
+        }
+        private bool LastBallInFrame(int pins)
+        {
+            return Strike(pins) || (!IsFistThrow);
+        }
         public void Add(int pins)
         {
-            score += pins;
+            scorer.AddThrow(pins);
+            AdjustCurrentFrame(pins);
+        }
+        private void AdjustCurrentFrame(int pins)
+        {
+            if (LastBallInFrame(pins))
+            {
+                AdvanceFrame();
+            }
+            else
+            {
+                IsFistThrow = false;
+            }
+        }
+        private void AdvanceFrame()
+        {
+            currentFrame++;
+            if (currentFrame > 10)
+            {
+                currentFrame = 10;
+            }
+        }
+        public int ScoreForFrame(int theFrame)
+        {
+            return scorer.ScoreForFrame(theFrame);
         }
     }
 }
--- TDD/GameTests.cs
+++ TDD/GameTests.cs
@@ -6,12 +6,123 @@ namespace TDD
     [TestFixture]
     public class GameTests
     {
-        [Test]
-        public void TestOneThrow()
+        private Game game;
+
+        [SetUp]
+        public void SetUp()
         {
-            Game game = new Game();
+            game = new Game();
+        }
+        [Test]
+        public void TestTwoThrowsNoMark()
+        {
             game.Add(5);
-            Assert.AreEqual(5, game.Score);
+            game.Add(4);
+            Assert.AreEqual(9, game.Score);
+        }
+        [Test]
+        public void TestFourThrowsNoMark()
+        {
+            game.Add(5);
+            game.Add(4);
+            game.Add(7);
+            game.Add(2);
+            Assert.AreEqual(18, game.Score);
+            Assert.AreEqual(9, game.ScoreForFrame(1));
+            Assert.AreEqual(18, game.ScoreForFrame(2));
+        }
+        [Test]
+        public void TestSimpleSpare()
+        {
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            Assert.AreEqual(13, game.ScoreForFrame(1));
+        }
+        [Test]
+        public void TestSimpleFrameAfterSpare()
+        {
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            game.Add(2);
+            Assert.AreEqual(13, game.ScoreForFrame(1));
+            Assert.AreEqual(18, game.ScoreForFrame(2));
+            Assert.AreEqual(18, game.Score);
+        }
+        [Test]
+        public void TestSimpleStrike()
+        {
+            game.Add(10);
+            game.Add(3);
+            game.Add(6);
+            Assert.AreEqual(19, game.ScoreForFrame(1));
+            Assert.AreEqual(28, game.Score);
+        }
+        [Test]
+        public void TestPerfectGame()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                game.Add(10);
+            }
+            Assert.AreEqual(300, game.Score);
+        }
+        [Test]
+        public void TestEndOfArray()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                game.Add(0);
+                game.Add(0);
+            }
+            game.Add(2);
+            game.Add(8); // спэа в 10-м фрейме
+            game.Add(10); // страйк в последней позиции массива
+            Assert.AreEqual(20, game.Score);
+        }
+        [Test]
+        public void TestSampleGame()
+        {
+            game.Add(1);
+            game.Add(4);
+            game.Add(4);
+            game.Add(5);
+            game.Add(6);
+            game.Add(4);
+            game.Add(5);
+            game.Add(5);
+            game.Add(10);
+            game.Add(0);
+            game.Add(1);
+            game.Add(7);
+            game.Add(3);
+            game.Add(6);
+            game.Add(4);
+            game.Add(10);
+            game.Add(2);
+            game.Add(8);
+            game.Add(6);
+            Assert.AreEqual(133, game.Score);
+        }
+        [Test]
+        public void TestHeartBreak()
+        {
+            for (int i = 0; i < 11; i++)
+                game.Add(10);
+            game.Add(9);
+            Assert.AreEqual(299, game.Score);
+        }
+        [Test]
+        public void TestTenthFrameSpare()
+        {
+            for (int i = 0; i < 9; i++)
+                game.Add(10);
+            game.Add(9);
+            game.Add(1);
+            game.Add(1);
+            Assert.AreEqual(270, game.Score);
         }
     }
 }
+
