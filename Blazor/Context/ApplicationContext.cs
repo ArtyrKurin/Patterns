0d38d419a9ee02dcb933fb4a66b016db9ae2876a
artyrkurin <kurinartyr@gmail.com>
2021-06-09 17:20:18 +0300

    Add Blazor identity

---
+++ Blazor/Context/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Blazor.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor.Context
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ MobileApp/MobileApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace MobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StartPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
